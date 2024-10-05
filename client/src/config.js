7b494f4462c61e0d5e96306203d599eab105060e
ADARSH T P <136061810+atp-adarshtp@users.noreply.github.com>
2024-10-05 11:12:22 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -7,7 +7,6 @@ const config = {
     REACT_APP_REMOTE_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
     REACT_APP_LOCAL_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
     REACT_APP_LOCAL_NOTE_EDIT_URL:'http://157.90.18.105:5000/api/collections',
-  };
+};
   
-  export default config;
-  
+export default config;
