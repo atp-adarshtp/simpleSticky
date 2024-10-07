7ba60da1a913fef69ceec3573dade781048137d6
ADARSH T P <136061810+atp-adarshtp@users.noreply.github.com>
2024-10-07 15:53:46 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,12 @@
 // src/config.js
 const config = {
     REACT_APP_REMOTE_SIGNUP_URL: 'http://157.90.18.105:8081/api/signup',
-    REACT_APP_LOCAL_SIGNUP_URL: 'http://157.90.18.105:5000/api/auth/signup',
+    REACT_APP_LOCAL_SIGNUP_URL: 'http://157.90.18.105/api/auth/signup',
     REACT_APP_REMOTE_COLLECTIONS_URL: 'http://157.90.18.105:8081/api/collections',
-    REACT_APP_LOCAL_COLLECTIONS_URL: 'http://157.90.18.105:5000/api/collections',
-    REACT_APP_REMOTE_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
-    REACT_APP_LOCAL_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
-    REACT_APP_LOCAL_NOTE_EDIT_URL:'http://157.90.18.105:5000/api/collections',
+    REACT_APP_LOCAL_COLLECTIONS_URL: 'http://157.90.18.105/api/collections',
+    REACT_APP_REMOTE_LOGIN_URL: 'http://157.90.18.105/api/auth/signin',
+    REACT_APP_LOCAL_LOGIN_URL: 'http://157.90.18.105/api/auth/signin',
+    REACT_APP_LOCAL_NOTE_EDIT_URL:'http://157.90.18.105/api/collections',
 };
   
 export default config;
