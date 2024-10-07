4bf5404ebc6bc157aa7e89ebd8e12344c68d0806
ADARSH T P <136061810+atp-adarshtp@users.noreply.github.com>
2024-10-07 14:21:47 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@
 const config = {
     REACT_APP_REMOTE_SIGNUP_URL: 'http://157.90.18.105:8081/api/signup',
     REACT_APP_LOCAL_SIGNUP_URL: 'http://157.90.18.105:5000/api/auth/signup',
-    REACT_APP_REMOTE_COLLECTIONS_URL: 'http://157.90.18.105:5000:8081/api/collections',
+    REACT_APP_REMOTE_COLLECTIONS_URL: 'http://157.90.18.105:8081/api/collections',
     REACT_APP_LOCAL_COLLECTIONS_URL: 'http://157.90.18.105:5000/api/collections',
     REACT_APP_REMOTE_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
     REACT_APP_LOCAL_LOGIN_URL: 'http://157.90.18.105:5000/api/auth/signin',
