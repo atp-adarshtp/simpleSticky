4edb01c330be7f73d9daa2680e327a2ea1491985
ADARSH T P <136061810+atp-adarshtp@users.noreply.github.com>
2024-10-07 17:00:06 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,12 @@
 // src/config.js
 const config = {
     REACT_APP_REMOTE_SIGNUP_URL: 'http://ss.simplesticky.com:8081/api/signup',
-    REACT_APP_LOCAL_SIGNUP_URL: 'http://ss.simplesticky.com/api/auth/signup',
+    REACT_APP_LOCAL_SIGNUP_URL: 'http://ss.simplesticky.com:5000/api/auth/signup',
     REACT_APP_REMOTE_COLLECTIONS_URL: 'http://ss.simplesticky.com:8081/api/collections',
-    REACT_APP_LOCAL_COLLECTIONS_URL: 'http://ss.simplesticky.com/api/collections',
-    REACT_APP_REMOTE_LOGIN_URL: 'http://ss.simplesticky.com/api/auth/signin',
-    REACT_APP_LOCAL_LOGIN_URL: 'http://ss.simplesticky.com/api/auth/signin',
-    REACT_APP_LOCAL_NOTE_EDIT_URL:'http://ss.simplesticky.com/api/collections',
+    REACT_APP_LOCAL_COLLECTIONS_URL: 'http://ss.simplesticky.com:5000/api/collections',
+    REACT_APP_REMOTE_LOGIN_URL: 'http://ss.simplesticky.com:5000/api/auth/signin',
+    REACT_APP_LOCAL_LOGIN_URL: 'http://ss.simplesticky.com:5000/api/auth/signin',
+    REACT_APP_LOCAL_NOTE_EDIT_URL:'http://ss.simplesticky.com:5000/api/collections',
 };
   
 export default config;
