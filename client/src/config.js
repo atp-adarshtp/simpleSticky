909ade93d51e5db943e4ff943210661069788190
ADARSH T P <136061810+atp-adarshtp@users.noreply.github.com>
2026-03-06 16:11:39 +0530

    46.4.17.117

--- client/src/config.js
+++ client/src/config.js
@@ -1,14 +1,11 @@
 // src/config.js
 const config = {
-    // REACT_APP_REMOTE_SIGNUP_URL: 'http://10.10.10.183:8081/api/auth/signup',
-    REACT_APP_LOCAL_SIGNUP_URL: 'http://localhost:5000/api/auth/signup',
-    // REACT_APP_REMOTE_COLLECTIONS_URL: 'http://10.10.10.183:8081/api/collections',
-    REACT_APP_LOCAL_COLLECTIONS_URL: 'http://localhost:5000/api/collections',
-    // REACT_APP_REMOTE_LOGIN_URL: 'http://10.10.10.183:8081/api/auth/signin',
-    REACT_APP_LOCAL_LOGIN_URL: 'http://localhost:5000/api/auth/signin',
-    REACT_APP_LOCAL_NOTE_EDIT_URL:'http://localhost:5000/api/collections',
-    REACT_APP_LOCAL_COLLECTIONS_SHARED_URL: "http://localhost:5000/api/collections/shared",
-    REACT_APP_LOCAL_COLLECTION_SHARE_URL: "http://localhost:5000/api/collections",
+    REACT_APP_LOCAL_SIGNUP_URL: 'https://46.4.17.117:5000/api/auth/signup',
+    REACT_APP_LOCAL_COLLECTIONS_URL: 'https://46.4.17.117:5000/api/collections',
+    REACT_APP_LOCAL_LOGIN_URL: 'https://46.4.17.117:5000/api/auth/signin',
+    REACT_APP_LOCAL_NOTE_EDIT_URL:'https://46.4.17.117:5000/api/collections',
+    REACT_APP_LOCAL_COLLECTIONS_SHARED_URL: "https://46.4.17.117:5000/api/collections/shared",
+    REACT_APP_LOCAL_COLLECTION_SHARE_URL: "https://46.4.17.117:5000/api/collections",
   };
   
   export default config;
